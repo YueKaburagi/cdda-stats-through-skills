f40337d9f1596095c5fd33d9f56f4c539cbec93a
YueKaburagi <class_mercurycc@yahoo.co.jp>
2016-01-29 12:01:44 +0900

    fix some function calls based new class_definitions.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 --Increasing stats by skill
 local MOD = {
+    supportedVersion = "cdda0.C-4253",
     str_skills = {"carpentry", "mechanics", "swimming", "bashing", "melee", "throw"},
     dex_skills = {"driving", "survival", "tailor", "traps", "dodge", "stabbing", "unarmed"},
     int_skills = {"barter", "computer", "cooking", "electronics", "fabrication", "firstaid", "speech"},
@@ -12,10 +13,10 @@ mods["yk_stats_through_skills"] = MOD
 function MOD.on_day_passed()
     game.add_msg("Calculating new stats based off skills")
 
-    setStatBonus("exp_str", calculateStatBonus(MOD.str_skills, MOD.base_weight), "Str")
-    setStatBonus("exp_dex", calculateStatBonus(MOD.dex_skills, MOD.base_weight), "Dex")
-    setStatBonus("exp_int", calculateStatBonus(MOD.int_skills, MOD.base_weight), "Int")
-    setStatBonus("exp_per", calculateStatBonus(MOD.per_skills, MOD.base_weight), "Per")
+    setStatBonus(efftype_id("exp_str"), calculateStatBonus(MOD.str_skills, MOD.base_weight), "Str")
+    setStatBonus(efftype_id("exp_dex"), calculateStatBonus(MOD.dex_skills, MOD.base_weight), "Dex")
+    setStatBonus(efftype_id("exp_int"), calculateStatBonus(MOD.int_skills, MOD.base_weight), "Int")
+    setStatBonus(efftype_id("exp_per"), calculateStatBonus(MOD.per_skills, MOD.base_weight), "Per")
 end
 
 function calculateStatBonus(aSkills, iBase)
@@ -31,12 +32,16 @@ function calculateStatBonus(aSkills, iBase)
     return skillBonus
 end
 
-function setStatBonus(sEffect, iBonus, sStatIdent)
-    local iBonusOld = player:get_effect_int(sEffect, "num_bp")
-    if not player:has_effect(sEffect, "num_bp") then
+function setStatBonus(idEffect, iBonus, sStatIdent)
+    local iBonusOld = player:get_effect_int(idEffect, "num_bp")
+    if not player:has_effect(idEffect, "num_bp") then
         iBonusOld = 0
     end
-    player:add_effect(sEffect, 1, "num_bp", true, iBonus)
+    if (iBonus > 0) then
+        player:add_effect(idEffect, 1, "num_bp", true, iBonus)
+    else
+        player:remove_effect(idEffect, "num_bp")
+    end
     print_results(iBonus, sStatIdent, iBonusOld)
 end
 
